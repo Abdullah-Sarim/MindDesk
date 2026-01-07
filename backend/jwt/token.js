5c1bd7be826a6fd48602ce9845f2b9fcef95ff43
Sarim Abdullah <abdullahsarim1100@gmail.com>
2026-01-08 01:27:37 +0530

    setting cookie for both backend and frontend use

--- backend/jwt/token.js
+++ backend/jwt/token.js
@@ -1,18 +1,21 @@
 import jwt from "jsonwebtoken";
 import User from "../model/user.model.js";
+
 export const generateTokenAndSaveInCookies = async (userId, res) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
-    expiresIn: "5d",
-    // expires: new Date(Date.now() + 5 * 24 * 60 * 60 * 1000)  // 5 days expiry time
-  });
+  const token = jwt.sign(
+    { userId },
+    process.env.JWT_SECRET_KEY,
+    { expiresIn: "5d" }
+  );
+
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.MODE != devlopment,
-    sameSite: process.env.MODE = "development" ? "lax" : "none",
-    maxAge: 5*24*60*1000,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 5 * 24 * 60 * 60 * 1000, // ✅ 5 days
     path: "/",
   });
 
-  await User.findByIdAndUpdate(userId, { token });
+
   return token;
 };
